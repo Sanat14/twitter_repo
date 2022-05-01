f3004c4421194429d7c50b8e82d5a51dd9645050
Bhavya2002 <bhatiabhavya.02@gmail.com>
2022-05-01 23:53:01 +0100

    modified menu.c

--- menu.c
+++ menu.c
@@ -1,46 +1,62 @@
-
 #include "twitter_create.h"
 #include "functions.h"
 #include "menu.h"
 #include <stdio.h>
 
 int menu(twitter *ts, user *sptr){
+
     int choice = -1;
     instructions();
+
+    //calling functions according to user choice
     while(choice != 0){
+
         printf("Enter your choice:\n");
         scanf("%d", &choice);
         fflush(stdin);
-        switch (choice) {
+
+        switch (choice){
+
             case 1:
-                postTweet(ts,sptr);
+                postTweet(ts, sptr);
                 return 1;
+
             case 2:
                 getNewsFeed(ts, sptr);
                 return 2;
+
             case 3:
                 follow(ts, sptr);
                 return 3;
+
             case 4:
                 unfollow(ts, sptr);
                 return 4;
+
             case 5:
                 delete(ts, sptr);
                 return 6;
+
             case 6:
                 return 6;
+
             case 7:
                 return 7;
+
             default:
-                printf("Please select another option\n");
+                printf("Please select another option!\n");
                 continue;
+
         }
+
     }
+
     return 7;
+
 }
 
-void instructions(void)
-{
+void instructions(void){
+
     printf("Option         Function\n");
     printf(" 1        |    postTweet\n");
     printf(" 2        |    getNewsFeed\n");
