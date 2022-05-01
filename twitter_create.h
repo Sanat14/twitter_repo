739ebd54e97277642687938370d9bbcd7c86de13
Bhavya2002 <bhatiabhavya.02@gmail.com>
2022-05-01 23:55:42 +0100

    modified twitter_create.h

--- twitter_create.h
+++ twitter_create.h
@@ -16,30 +16,34 @@
 
 
 typedef struct tweet{
+
     int id;
     char msg[TWEET_LENGTH];
     char user[USR_LENGTH];
     struct tweet *nextPtr;
+
 }tweet;
 
 typedef tweet *tweetPtr;
 
 typedef struct user{
+
     char username[USR_LENGTH];
     char followers[MAX_FOLLOWERS][USR_LENGTH];
     int num_followers;
     char following[MAX_FOLLOWING][USR_LENGTH];
     int num_following;
     struct user *nextptr;
+
 }user;
 
 typedef user *userPtr;
 
+typedef struct twitter{
 
-typedef struct twitter
-{
     userPtr headPtr;
     tweetPtr tweetHeadPtr;
+
 } twitter;
 
 void create_twitter_system(twitter * ts); 
