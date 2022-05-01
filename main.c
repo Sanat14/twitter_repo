f6e4a21c717dc51f1b142de80005a218d46e880a
Bhavya2002 <bhatiabhavya.02@gmail.com>
2022-05-01 23:50:34 +0100

    Updated main.c

--- main.c
+++ main.c
@@ -6,18 +6,25 @@
 int main()
 {
     twitter ts;
-    create_twitter_system(&ts); //calling the function
-    userPtr currptr = ts.headPtr; //calling the headptr inside the twitter system
-    printf("Last Active Users\n");
+    create_twitter_system(&ts); // Calling the function
+    userPtr currptr = ts.headPtr; // Calling the headPtr inside the twitter system
+    printf("Last Active Users: \n");
     puts("--------------------------------------------------------------------------");
-    if(ts.headPtr == NULL)
-    {
+
+    if(ts.headPtr == NULL){
+
         exit(EXIT_SUCCESS);
+
     }
+
+    // Prints out all the users linked list style
     while(currptr != NULL){
+
         printf("User:%s\t\t  Following:%d\t    Followers:%d\n", currptr->username, currptr->num_following, currptr->num_followers);
         currptr = currptr->nextptr;
-    }
-    return 0;
-}
 
+    }
+
+    return 0;
+
+}
